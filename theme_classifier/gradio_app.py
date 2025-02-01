791e6045a131a47dafed29db39c10aabb7f8758c
Yuvi-me-code <yuvrajaparmar@gmail.com>
2025-02-01 18:31:55 +0530

    gradio changes to perfect input themes and minor changes

--- theme_classifier/gradio_app.py
+++ theme_classifier/gradio_app.py
@@ -1,24 +1,29 @@
 import gradio as gr
 import matplotlib.pyplot as plt
 from theme_classifier import ThemeClassifier
+import os
+import pandas as pd
 
-def get_themes(theme_list_str, subtitles_path, save_path):
-    theme_list = [theme.strip() for theme in theme_list_str.split(',')]  # Trim spaces
+def get_themes(theme_list_str,subtitles_path,save_path):
+    theme_list_str = ",".join(word.strip().title() for word in theme_list_str.replace(" ", "").split(","))
+    theme_list = theme_list_str.split(',')
     theme_classifier = ThemeClassifier(theme_list)
-    output_data = theme_classifier.get_themes(subtitles_path, save_path)
+    output_data = theme_classifier.get_themes(subtitles_path,save_path)
 
-    # Ensure theme columns exist, filling missing themes with 0
-    output_data = output_data.reindex(columns=theme_list, fill_value=0).sum().reset_index()
-    output_data.columns = ['Theme', 'Score']
+    # Remove dialogue from the theme list
+    theme_list = [theme for theme in theme_list if theme != 'dialogue']
+    output_data = output_data[theme_list]
 
-    # Create a bar plot using Matplotlib
+    output_data = output_data[theme_list].sum().reset_index()
+    output_data.columns = ['Theme','Score']
+
     fig, ax = plt.subplots(figsize=(6, 6))
     ax.barh(output_data["Theme"], output_data["Score"], color="skyblue")
     ax.set_xlabel("Score")
     ax.set_ylabel("Theme")
     ax.set_title("Series' Themes")
 
-    return fig  # Return the Matplotlib figure for gr.Plot()
+    return fig  
 
 def main():
     with gr.Blocks() as iface:
--- theme_classifier/theme_classifier.py
+++ theme_classifier/theme_classifier.py
@@ -1,74 +1,80 @@
-import torch
 from transformers import pipeline
+import torch
 from nltk.tokenize import sent_tokenize
-import numpy as np
+import nltk
 import pandas as pd
+import numpy as np
 import os
 import sys
-import pathlib
-import nltk
-nltk.download('punkt_tab')
+import pathlib 
 folder_path = pathlib.Path(__file__).parent.resolve()
-sys.path.append(os.path.join(folder_path, '../'))
+sys.path.append(os.path.join(folder_path,'../'))
 from utils import load_subtitles_dataset
+nltk.download('punkt')
+nltk.download('punkt_tab')
 
 class ThemeClassifier():
     def __init__(self, theme_list):
         self.model_name = "facebook/bart-large-mnli"
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.device = 0 if torch.cuda.is_available() else 'cpu'
         self.theme_list = theme_list
         self.theme_classifier = self.load_model(self.device)
-
-    model_name = "facebook/bart-large-mnli"
-
-    def load_model(self, device):
+    
+    def load_model(self,device):
         theme_classifier = pipeline(
             "zero-shot-classification",
             model=self.model_name,
-            device = device
+            device=device
         )
 
         return theme_classifier
-    
+
     def get_themes_inference(self, script):
         script_sentences = sent_tokenize(script)
 
-        sentence_batch_size = 20
+        # Batch Sentence
+        sentence_batch_size=20
         script_batches = []
-        for index in range(0, len(script_sentences), sentence_batch_size):
-            sent = " ".join(script_sentences[index : index + sentence_batch_size])
+        for index in range(0,len(script_sentences),sentence_batch_size):
+            sent = " ".join(script_sentences[index:index+sentence_batch_size])
             script_batches.append(sent)
-
         
+        # Run Model
         theme_output = self.theme_classifier(
             script_batches,
-            self.theme_list, 
-            multi_label = True
+            self.theme_list,
+            multi_label=True
         )
 
+        # Wrangle Output 
         themes = {}
         for output in theme_output:
-            for label, score in zip(output['labels'], output['scores']):
+            for label,score in zip(output['labels'],output['scores']):
                 if label not in themes:
                     themes[label] = []
                 themes[label].append(score)
-        
-        themes = {key : np.mean(np.array(value)) for key, value in themes.items()}
+
+        themes = {key: np.mean(np.array(value)) for key,value in themes.items()}
 
         return themes
-    
-    def get_themes(self, dataset_path, save_path = None):
 
+    def get_themes(self,dtaset_path, save_path=None):
+        # Read Save Output if Exists
         if save_path is not None and os.path.exists(save_path):
-            data = pd.read_csv(save_path)
-            return data
-        
-        data = load_subtitles_dataset(dataset_path)
-        output_themes = data['scripts'].apply(self.get_themes_inference)
-        themes_data = pd.DataFrame(output_themes.tolist())
-        data[themes_data.columns] = themes_data
+            df = pd.read_csv(save_path)
+            return df
 
-        if save_path is not None and save_path != "":
-            data.to_csv(save_path, index = False)
+        # load Dataset
+        df = load_subtitles_dataset(dtaset_path)
+
+        # Run Inference
+        output_themes = df['scripts'].apply(self.get_themes_inference)
+
+        themes_df = pd.DataFrame(output_themes.tolist())
+        df[themes_df.columns] = themes_df
+
+        # Save output
+        if save_path is not None:
+            df.to_csv(save_path,index=False)
         
-        return data
+        return df
